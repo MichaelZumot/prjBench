26fda8dd1a8cf946173bf13e70ae9b08c2d04194
Michael Zumot <michael.zumot@gmail.com>
2023-11-27 10:02:30 +0100

    first push

---
+++ src/main/java/com/inetum/prjBench/PrjBenchApplication.java
@@ -0,0 +1,13 @@
+package com.inetum.prjBench;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjBenchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjBenchApplication.class, args);
+	}
+
+}
