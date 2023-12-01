2f3083bd2afbb946fa13c96f8cf4ae1a318ea868
Michael Zumot <michael.zumot@inetum-realdolmen.world>
2023-12-01 12:04:28 +0100

    run file

--- runprj.bat
+++ runprj.bat
@@ -4,6 +4,7 @@
 cd .\backend
 :: Restart Docker Compose services
 echo running docker-compose.yaml file 
+    docker compose -f docker-compose.yaml down 
     docker compose -f docker-compose.yaml up -d --build 
 
 :: Change directory to \backend and run mvn spring-boot:run
